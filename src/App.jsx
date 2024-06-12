a1bff8ec997803e5ad56bfd70f656e5c5ae4cbe2
Alex Okeyo <alexokeyo15@gmail.com>
2024-06-12 16:26:21 +0300

    hero section development

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <>
       <Navbar />
-      <div className="max-w-7xl mx-auto pt-20 px-6">
+      <div className="max-w-5xl mx-auto pt-20 px-6">
       <HeroSection />
       </div>
     </>
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,14 +4,14 @@ const HeroSection = () => {
   return (
     <div className="container mx-auto p-4">
     <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-      <div className="flex flex-col justify-center">
-        <h1 className="text-3xl font-bold mb-2">Investing</h1>
-        <h1 className="text-3xl font-bold mb-2">Made</h1>
-        <h1 className="text-3xl font-bold mb-2">Simple</h1>
-        <p className="text-lg">Riven is the easiest way to access smarter investment options & earn real returns.</p>
+      <div className="flex flex-col justify-center items-start">
+        <h1 className="text-7xl font-bold mb-2">Investing</h1>
+        <h1 className="text-7xl font-bold mb-2">Made</h1>
+        <h1 className="text-7xl font-bold mb-2">Simple</h1>
+        <p className="text-2xl pt-5">Riven is the easiest way to access smarter investment options & earn real returns.</p>
       </div>
-      <div className="flex justify-center">
-        
+      <div className="flex">
+      <img src="/src/assets/img/Group9629.svg" alt="Placeholder" class="w-full h-auto " />
       </div>
     </div>
   </div>
