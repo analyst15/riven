6d10cd298616745aee9c52077fe3f4bdcae64a87
Alex Okeyo <alexokeyo15@gmail.com>
2024-06-12 13:32:21 +0300

    hero section

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <div className="container mx-auto p-4">
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+      <div className="flex flex-col justify-center">
+        <h1 className="text-3xl font-bold mb-2">Investing</h1>
+        <h1 className="text-3xl font-bold mb-2">Made</h1>
+        <h1 className="text-3xl font-bold mb-2">Simple</h1>
+        <p className="text-lg">Riven is the easiest way to access smarter investment options & earn real returns.</p>
+      </div>
+      <div className="flex justify-center">
+        
+      </div>
+    </div>
+  </div>
+  )
+}
+
+export default HeroSection
