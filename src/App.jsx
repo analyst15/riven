801705bb1069e2fb964ec450e30b6fd6ec575734
Alex Okeyo <alexokkeyo15@gmail.com>
2024-06-13 22:47:39 +0300

    StartInvesting

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import HeroSection from './components/HeroSection'
 import MobileSection from './components/MobileSection'
+import StartInvesting from './components/StartInvesting'
 import Navbar from './components/Navbar'
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
       <div className="max-w-5xl mx-auto pt-20 px-6">
       <HeroSection />
       <MobileSection />
+      <StartInvesting />
       </div>
     </>
   )
---
+++ src/components/StartInvesting.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const StartInvesting = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default StartInvesting
