92f2b0a31643154156c03d260a03ac03550c36a0
Alex Okeyo <alexokeyo15@gmail.com>
2024-06-14 16:59:31 +0300

    OurProducts cards

--- src/components/OurProducts.jsx
+++ src/components/OurProducts.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const OurProducts = () => {
   return (
-    <div>OurProducts</div>
+    <div className="mt-16 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4 p-4">
+    <div className="bg-gray-100 p-4">Stocks</div>
+    <div className="bg-gray-100 p-4">Exchange Traded Funds (ETFs)</div>
+    <div className="bg-gray-100 p-4">For Institutions</div>
+
+</div>
+ 
   )
 }
 
