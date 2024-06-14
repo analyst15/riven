db657dc17c0d18a2ac9b1070ddb910430b0a64ed
Alex Okeyo <alexokeyo15@gmail.com>
2024-06-14 16:44:42 +0300

    RivenAcademy button styling

--- src/components/RivenAcademy.jsx
+++ src/components/RivenAcademy.jsx
@@ -5,8 +5,8 @@ const MobileSection = () => {
         <div className="container mx-auto flex flex-col md:flex-row h-screen">
             <div className="content py-16 flex flex-col justify-center w-full md:w-1/2">
             <h1 className="font-bold text-xl">Riven Academy</h1>
-                <p className="text-base pt-8">Become a better investor on the go, right on the app. Here is a preview of the things you can learn when you sign up</p>
-                <a href="#" className="bg-black text-white text-center py-2 px-4 rounded focus:outline-none w-1/3">Start Investing</a>
+                <p className="text-base py-8">Become a better investor on the go, right on the app. Here is a preview of the things you can learn when you sign up</p>
+                <a href="#" className="bg-black text-white text-center py-2 px-4 rounded focus:outline-none w-2/3">Sign Up to access Riven Academy</a>
             </div>
                 <div className="flex">
                 <img src="/src/assets/img/MaskGroup1.png" alt="Investment app screenshot" className="w-full h-auto" />
