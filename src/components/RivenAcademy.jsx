c0bfa7e977c1f92a73a83cf838b815488249e164
Alex Okeyo <alexokeyo15@gmail.com>
2024-06-14 16:39:59 +0300

    RivenAcademy Changes

--- src/components/RivenAcademy.jsx
+++ src/components/RivenAcademy.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 
-const RivenAcademy = () => {
+const MobileSection = () => {
   return (
-    <div>RivenAcademy</div>
+        <div className="container mx-auto flex flex-col md:flex-row h-screen">
+            <div className="content py-16 flex flex-col justify-center w-full md:w-1/2">
+            <h1 className="font-bold text-xl">Riven Academy</h1>
+                <p className="text-base pt-8">Become a better investor on the go, right on the app. Here is a preview of the things you can learn when you sign up</p>
+                <a href="#" className="bg-black text-white text-center py-2 px-4 rounded focus:outline-none w-1/3">Start Investing</a>
+            </div>
+                <div className="flex">
+                <img src="/src/assets/img/MaskGroup1.png" alt="Investment app screenshot" className="w-full h-auto" />
+                </div>
+        </div>    
   )
 }
 
-export default RivenAcademy
+export default MobileSection
